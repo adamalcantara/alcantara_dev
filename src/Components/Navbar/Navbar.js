e9a16d845304b92e7549bd5047dc9d10c49fcb49
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-16 21:59:47 -0400

    Added navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../../Assets/logo.png'
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar

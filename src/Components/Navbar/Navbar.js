ceaec1ed46c9ee930fad58c46d61d8df80e9faf4
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-17 21:44:28 -0400

    Added burger

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -13,6 +13,11 @@ const Navbar = () => {
             <Link to="/" id="logoimg">
                 <img src={Logo} alt="logo" id="logo"></img>
             </Link>
+            <div className="burger">
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
             <nav id="navbar">
                 <div id="links">
                     <Link to="/" className="links">Portfolio</Link>

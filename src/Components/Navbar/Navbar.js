67135fa7af34a2972c67abe6b12569d2c346edf0
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-17 20:47:35 -0400

    Added basic code to pages

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,9 +4,18 @@ import Logo from '../../Assets/img/logo.png'
 
 const Navbar = () => {
     return (
-        <Link to="/" id="logoimg">
-            <img src={Logo} alt="logo" id="logo"></img>
-        </Link>
+        <div>
+            <Link to="/" id="logoimg">
+                <img src={Logo} alt="logo" id="logo"></img>
+            </Link>
+            <nav id="navbar">
+                <div id="navbar">
+                    <Link to="/" className="links">Portfolio</Link>
+                    <Link to="/about" className="links">About</Link>
+                    <Link to="/contact" className="links">Contact</Link>
+                </div>
+            </nav>
+        </div>
     )
 }
 

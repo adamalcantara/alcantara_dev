0249058bef26eb14e70001ef06524ab8f16a416d
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-18 20:58:57 -0400

    mode held in local storage

--- src/Styles/globalStyles.js
+++ src/Styles/globalStyles.js
@@ -4,6 +4,7 @@ export const GlobalStyles = createGlobalStyle`
 body {
     background-color: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
+    transition: all .5s linear;
 }
 
 `;
--- src/Styles/useDarkMode.js
+++ src/Styles/useDarkMode.js
@@ -1,10 +1,20 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 export const useDarkMode = () => {
     const [theme, setTheme ] = useState('dark');
 
-    const toggleTheme = () => {
-        theme === 'dark' ? setTheme('light') : setTheme('dark');
+    const setMode = mode => {
+        window.localStorage.setItem('theme', mode);
+        setTheme(mode);
     }
+
+    const toggleTheme = () => {
+        theme === 'dark' ? setMode('light') : setMode('dark');
+    }
+
+    useEffect(() => {
+        const localTheme = window.localStorage.getItem('theme');
+        localTheme ? setTheme(localTheme) : setMode('dark');
+    }, []);
     return [ theme, toggleTheme];
 }

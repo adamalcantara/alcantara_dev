9ab138519ea014194a2ce9ffbbe387d69b9c04ad
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-18 20:08:26 -0400

    Installed styled components, starting on dark mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import styled from "styled-components";
 import Navbar from "./Components/Navbar/Navbar";
 import Portfolio from "./Pages/Portfolio/Portfolio";
 import About from "./Pages/About/About";
 import Contact from "./Pages/Contact/Contact"
 
+const Container = styled.div`
+
+`;
 
 function App() {
   return (
+    <Container>
     <Router>
       <div className="page">
         <Navbar />
@@ -20,6 +25,7 @@ function App() {
       </div>
 
     </Router>
+    </Container>
     
   );
 }

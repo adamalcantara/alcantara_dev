ab5be57e7b75aa487936d48c0fc3992dc5775fd0
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-19 19:34:17 -0400

    Styled content

--- src/Components/Form/ContactForm.js
+++ src/Components/Form/ContactForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
+import "./ContactForm.css"
 
 function ContactForm() {
   const [state, handleSubmit] = useForm("xbjqkvbl");
@@ -10,31 +11,36 @@ function ContactForm() {
     <div>
       <p>Use the form below to contact me. I look foward to hearing from you!</p>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="email">
-          Email Address
-        </label>
-        <input
-          id="email"
-          type="email"
-          name="email"
-        />
-        <ValidationError
-          prefix="Email"
-          field="email"
-          errors={state.errors}
-        />
-        <textarea
-          id="message"
-          name="message"
-        />
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-        />
-        <button type="submit" disabled={state.submitting}>
-          Submit
-        </button>
+        <div id="contactForm">
+          <label htmlFor="email">
+            Email Address
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+          />
+          <ValidationError
+            prefix="Email"
+            field="email"
+            errors={state.errors}
+          />
+          <label htmlFor="email">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+          <button type="submit" disabled={state.submitting} id="submitBtn">
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );

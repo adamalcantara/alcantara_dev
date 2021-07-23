df3fe9ded83f673e75f68c75f91a3bfa9cde5eff
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-22 21:52:35 -0400

    Styling added to modal

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
@@ -28,8 +28,8 @@ function Card() {
     const handleClose2 = () => setShowModal2(false)
 
     const cardInfo = [
-        { id: "stockchasr", text: "StockChasr", handleShow: handleShow1, handleClose: handleClose1, show: showModal1, modalTitle: "StockChasr", modalText: "StockChasr was a project that was my final project", img: StockChasrScreenshot },
-        { id: "andChill", text: "&Chill", handleShow: handleShow2, handleClose: handleClose2, show: showModal2, modalTitle: "&Chill", modalText: "&Chill Modal", img: andChillScreenshot },
+        { id: "stockchasr", text: "StockChasr", handleShow: handleShow1, handleClose: handleClose1, show: showModal1, modalTitle: "StockChasr", modalText1: "Using the Market Stack API, and the Polygon API, my final group project for my coding boot camp involved building a stock application that lets users create a watch list and track stocks they might want to buy.  The greatest undertaking for me in this project was creating the interactive chart that users see once they search for a stock.", modalText2: "StockChasr was built using React as the framework, and I was primarily responsible for the chart, which used CanvasJS, the look of the dashboard, which used Material UI, and the styling which was done with CSS.", img: StockChasrScreenshot, project: "https://stockchasr.herokuapp.com/", github: "https://github.com/adamalcantara/stockchasr" },
+        { id: "andChill", text: "&Chill", handleShow: handleShow2, handleClose: handleClose2, show: showModal2, modalTitle: "&Chill", modalText1: "Built primaily with Handlebars, this application allows users to sign up and create an account. Once signed up, users will be matched with other users whose movie watching preferences match their own.", modalText2: "&Chill was built using Handlebars. I was responsible for the routing of the Handlebars files, the back end work which was done with SQL, and some of the CSS", img: andChillScreenshot, project: "https://and--chill.herokuapp.com/", github: "https://github.com/adamalcantara/andChill" },
     ];
 
     const renderCard = (card, index) => {
@@ -44,17 +44,24 @@ function Card() {
                 </div>
 
                 <Modal show={card.show} onHide={card.handleClose} dialogClassName="modal-90w" className="modalArea">
-                        <Modal.Title className="modalBox">{card.modalTitle}</Modal.Title>
+                    <Modal.Title className="modalBox">{card.modalTitle}</Modal.Title>
                     <Modal.Body id="modaltext">
+                        <div id="imgDiv">
                         <img src={card.img} id="img"></img>
-                        <p>{card.modalText}</p>
+                        </div>
+                        <p>{card.modalText1}</p>
+                        <p>{card.modalText2}</p>
+                        <div id="modalBtns">
+                        <a href={card.project} target="_blank" className="modalBtns">Deployed Project</a>
+                        <a href={card.github} target="_blank" className="modalBtns">Github Repository</a>
+                        </div>
                     </Modal.Body>
                     <div id="modalFooter">
                         <Button variant="secondary" onClick={card.handleClose} id="modalBtn">
                             Close
                         </Button>
-                        </div>
-                        
+                    </div>
+
                 </Modal>
 
                 {/* <Modal show={showModal2} onHide={card.handleClose}>

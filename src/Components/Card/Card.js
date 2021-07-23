b4ae13c1de30929b93ed4e79d765aa9fff4f21e2
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-22 21:32:33 -0400

    Styling added to cards

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,16 +1,17 @@
 import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
-import Modal from 'react-bootstrap/Modal'
-import ModalHeader from 'react-bootstrap/ModalHeader'
-import ModalTitle from 'react-bootstrap/ModalTitle'
-import ModalBody from 'react-bootstrap/ModalBody'
-import ModalFooter from 'react-bootstrap/ModalFooter'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import ModalHeader from 'react-bootstrap/ModalHeader';
+import ModalTitle from 'react-bootstrap/ModalTitle';
+import ModalBody from 'react-bootstrap/ModalBody';
+import ModalFooter from 'react-bootstrap/ModalFooter';
 import "./Card.css";
 
 //image imports
-import StockChasrScreenshot from "../../Assets/screenshots/stockchasrscreenshot.png"
+import StockChasrScreenshot from "../../Assets/screenshots/stockchasrscreenshot.png";
+import andChillScreenshot from "../../Assets/screenshots/andchillscreenshot.png"
 
 function Card() {
     const [show, setShow] = useState(false);
@@ -28,7 +29,7 @@ function Card() {
 
     const cardInfo = [
         { id: "stockchasr", text: "StockChasr", handleShow: handleShow1, handleClose: handleClose1, show: showModal1, modalTitle: "StockChasr", modalText: "StockChasr was a project that was my final project", img: StockChasrScreenshot },
-        { id: "andChill", text: "&Chill", handleShow: handleShow2, handleClose: handleClose2, show: showModal2, modalTitle: "&Chill", modalText: "&Chill Modal" },
+        { id: "andChill", text: "&Chill", handleShow: handleShow2, handleClose: handleClose2, show: showModal2, modalTitle: "&Chill", modalText: "&Chill Modal", img: andChillScreenshot },
     ];
 
     const renderCard = (card, index) => {
--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from "../../Components/Card/Card"
+import "./Portfolio.css"
 
 const Portfolio = () => {
     return (
